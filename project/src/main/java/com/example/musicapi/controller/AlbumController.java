e585d154b90b19967adfeb474a7cd4e105ce2b14
ChristianDellarosa <Christiandellarosa98@gmail.com>
2020-03-09 18:11:40 -0300

    Add album controller

---
+++ project/src/main/java/com/example/musicapi/controller/AlbumController.java
@@ -0,0 +1,44 @@
+package com.example.musicapi.controller;
+
+import com.example.musicapi.entity.Album;
+import com.example.musicapi.service.Interface.IAlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/1")
+public class AlbumController {
+
+    @Autowired
+    private IAlbumService albumService;
+
+    @GetMapping("/album")
+    public List<Album> searchAll() {
+        return albumService.searchAll();
+    }
+
+    @GetMapping("/album/{id}")
+    public Album searchById(@PathVariable("id") int idAlbum) {
+        return albumService.searchById(idAlbum);
+    }
+
+    @PostMapping("/album")
+    public Album save(@RequestBody Album album) {
+        albumService.save(album);
+        return album;
+    }
+
+    @PutMapping("/album")
+    public Album update(@RequestBody Album album) {
+        albumService.save(album);
+        return album;
+    }
+
+    @DeleteMapping("/album/{id}")
+    public String delete(@PathVariable("id") int idAlbum) {
+        albumService.delete(idAlbum);
+        return "Registro Eliminado";
+    }
+}
