5c81c2af82e4c47378935fc3531b1483c34d5165
ChristianDellarosa <Christiandellarosa98@gmail.com>
2020-03-17 13:11:41 -0300

    Add Inversions Of Control for CONSTRUCTOR METHOD

--- project/src/main/java/com/example/musicapi/service/AlbumService.java
+++ project/src/main/java/com/example/musicapi/service/AlbumService.java
@@ -5,15 +5,18 @@ import com.example.musicapi.repository.IAlbumRepository;
 import com.example.musicapi.service.Interface.IAlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class AlbumService implements IAlbumService {
 
+    private final IAlbumRepository albumRepository;
+
     @Autowired
-    private IAlbumRepository albumRepository;
+    public AlbumService(IAlbumRepository albumRepository) { /*IOC for Constructor*/
+        this.albumRepository = albumRepository;
+    }
 
     @Override
     public List<Album> searchAll() {
